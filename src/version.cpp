0f3f6113c1cd38556a41d1bf3a33c7c5199e48b2
Alan Cramer <alan@cramer.id.au>
2018-03-20 20:11:38 +1030

    Version Correction

--- src/version.cpp
+++ src/version.cpp
@@ -38,8 +38,8 @@ const std::string CLIENT_NAME("ArtiqoxCore");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "789918f"
-#    define GIT_COMMIT_DATE "Sat, 27 Jan 2018 19:49:36 +1030"
+#    define GIT_COMMIT_ID "81b0683"
+#    define GIT_COMMIT_DATE "Tue, 20 Mar 2018 19:50:33 +1030"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
