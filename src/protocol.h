ea25499e0f2675c07175d65dd5281fab57286fe6
Alan Cramer <alan@cramer.id.au>
2018-09-16 12:29:26 +0930

    Add NODE_BLOOM service bit and bump protocol version
    Lets nodes advertise that they offer bloom filter support explicitly.
    The protocol version bump allows SPV nodes to assume that NODE_BLOOM is
    set if NODE_NETWORK is set for pre-70002 nodes.
    
    Also adds an undocumented option to turn bloom filter support off for
    testing purposes. Nodes attempting to use bloom filters are immediately
    dropped so as to not waste their bandwidth.
    
    ensure backward compatible service bits
    
    update protocol version to 70004
    
    Co-Authored-By: Peter Todd <pete@petertodd.org>

--- src/protocol.h
+++ src/protocol.h
@@ -68,17 +68,23 @@ public:
 /** nServices flags */
 enum {
     // NODE_NETWORK means that the node is capable of serving the block chain. It is currently
-    // set by all Bitcoin Core nodes, and is unset by SPV clients or other peers that just want
+    // set by all Artiqox Core nodes, and is unset by SPV clients or other peers that just want
     // network services but don't provide them.
     NODE_NETWORK = (1 << 0),
+
+    // NODE_BLOOM means the node is capable and willing to handle bloom-filtered connections.
+    // Artiqox Core will continue to support this by default, but will allow disabling it
+    // with the -bloomfilters option.
+    NODE_BLOOM = (1 << 1),
+
     // NODE_GETUTXO means the node is capable of responding to the getutxo protocol request.
-    // Bitcoin Core does not support this but a patch set called Bitcoin XT does.
+    // Artiqox Core does not support this but a patch set called Artiqox XT does.
     // See BIP 64 for details on how this is implemented.
-    NODE_GETUTXO = (1 << 1),
+    NODE_GETUTXO = (1 << 2),
 
     // Bits 24-31 are reserved for temporary experiments. Just pick a bit that
     // isn't getting used, or one not being used much, and notify the
-    // bitcoin-development mailing list. Remember that service bits are just
+    // artiqox-development mailing list. Remember that service bits are just
     // unauthenticated advertisements, so your code must be robust against
     // collisions and other cases where nodes may be advertising a service they
     // do not actually support. Other service bits should be allocated via the
