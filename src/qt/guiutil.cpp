3162362676b7ace326bebdd4db8d312d3b481f5c
Alan Cramer <alan@cramer.id.au>
2019-06-09 07:33:42 +0930

    Format URI without locale specific separator

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -190,9 +190,9 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 
 bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 {
-    // Convert bitcoin:// to bitcoin:
+    // Convert artiqox:// to artiqox:
     //
-    //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
+    //    Cannot handle this later, because artiqox:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
     if(uri.startsWith("artiqox://", Qt::CaseInsensitive))
     {
@@ -209,7 +209,9 @@ QString formatBitcoinURI(const SendCoinsRecipient &info)
 
     if (info.amount)
     {
-        ret += QString("?amount=%1").arg(BitcoinUnits::format(BitcoinUnits::BTC, info.amount, false, BitcoinUnits::separatorNever));
+        QLocale localeC(QLocale::c());
+        localeC.setNumberOptions(QLocale::OmitGroupSeparator | QLocale::RejectGroupSeparator);
+        ret += QString("?amount=%1").arg(BitcoinUnits::format(BitcoinUnits::BTC, info.amount, false, true, localeC));
         paramCount++;
     }
 
