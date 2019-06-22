039e1ef95d4affff309dccce4b370189cf78af46
Alan Cramer <alan@cramer.id.au>
2019-06-22 12:01:24 +0930

    Update languages to Version 2.1

--- src/qt/locale/bitcoin_sq.ts
+++ src/qt/locale/bitcoin_sq.ts
@@ -1,4 +1,4 @@
-<TS language="sq" version="2.0">
+<TS language="sq" version="2.1">
 <context>
     <name>AddressBookPage</name>
     <message>
@@ -22,12 +22,12 @@
         <translation>&amp;Kopjo</translation>
     </message>
     <message>
-        <source>&amp;Copy Address</source>
-        <translation>&amp;Kopjo adresen</translation>
-    </message>
-    <message>
         <source>Delete the currently selected address from the list</source>
         <translation>Fshi adresen e selektuar nga lista</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Eksporto të dhënat e skedës korrente në një skedar</translation>
     </message>
     <message>
         <source>&amp;Delete</source>
@@ -46,14 +46,42 @@
         <translation>Duke marr adresen</translation>
     </message>
     <message>
+        <source>These are your Artiqox addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Këto janë Artiqox adresat e juaja për të dërguar pagesa. Gjithmon kontrolloni shumën dhe adresën pranuese para se të dërgoni monedha.</translation>
+    </message>
+    <message>
+        <source>These are your Artiqox addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Këto janë Artiqox adresat e juaja për të pranuar pagesa. Rekomandohet që gjithmon të përdorni një adresë të re për çdo transaksion.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopjo adresen</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopjo &amp;Etiketë</translation>
+    </message>
+    <message>
         <source>&amp;Edit</source>
         <translation>&amp;Ndrysho</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Eksporto listën e adresave</translation>
     </message>
     <message>
         <source>Comma separated file (*.csv)</source>
         <translation>Skedar i ndarë me pikëpresje(*.csv)</translation>
     </message>
-    </context>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Eksportimi dështoj</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Gabim gjatë ruajtjes së listës së adresave në %1. Ju lutem provoni prapë.</translation>
+    </message>
+</context>
 <context>
     <name>AddressTableModel</name>
     <message>
@@ -145,7 +173,10 @@
     </message>
     </context>
 <context>
-    <name>BitcoinGUI</name>
+    <name>BanTableModel</name>
+    </context>
+<context>
+    <name>ArtiqoxGUI</name>
     <message>
         <source>Synchronizing with network...</source>
         <translation>Duke u sinkronizuar me rrjetin...</translation>
@@ -173,6 +204,10 @@
     <message>
         <source>&amp;Options...</source>
         <translation>&amp;Opsione</translation>
+    </message>
+    <message>
+        <source>&amp;Receiving addresses...</source>
+        <translation>Duke marr adresen</translation>
     </message>
     <message>
         <source>Change the passphrase used for wallet encryption</source>
@@ -215,18 +250,6 @@
         <translation>Shiriti i mjeteve</translation>
     </message>
     <message>
-        <source>Artiqox Core</source>
-        <translation>Berthama Artiqox</translation>
-    </message>
-    <message>
-        <source>&amp;About Artiqox Core</source>
-        <translation>Rreth Berthames Aiqkoin</translation>
-    </message>
-    <message>
-        <source>%1 and %2</source>
-        <translation>%1 dhe %2</translation>
-    </message>
-    <message>
         <source>%1 behind</source>
         <translation>%1 Pas</translation>
     </message>
@@ -262,9 +285,6 @@
         <source>Wallet is &lt;b&gt;encrypted&lt;/b&gt; and currently &lt;b&gt;locked&lt;/b&gt;</source>
         <translation>Portofoli po &lt;b&gt; enkriptohet&lt;/b&gt; dhe është &lt;b&gt; i kyçur&lt;/b&gt;</translation>
     </message>
-</context>
-<context>
-    <name>ClientModel</name>
     </context>
 <context>
     <name>CoinControlDialog</name>
@@ -354,16 +374,8 @@
 <context>
     <name>HelpMessageDialog</name>
     <message>
-        <source>Artiqox Core</source>
-        <translation>Berthama Artiqox</translation>
-    </message>
-    <message>
         <source>version</source>
         <translation>versioni</translation>
-    </message>
-    <message>
-        <source>About Artiqox Core</source>
-        <translation>Rreth Berthames Aiqkoin</translation>
     </message>
     </context>
 <context>
@@ -373,16 +385,15 @@
         <translation>Miresevini</translation>
     </message>
     <message>
-        <source>Welcome to Artiqox Core.</source>
-        <translation>Miresevini ne Berthamen Artiqox</translation>
-    </message>
-    <message>
-        <source>Artiqox Core</source>
-        <translation>Berthama Artiqox</translation>
-    </message>
-    <message>
         <source>Error</source>
         <translation>Problem</translation>
+    </message>
+    </context>
+<context>
+    <name>ModalOverlay</name>
+    <message>
+        <source>Form</source>
+        <translation>Formilarë</translation>
     </message>
     </context>
 <context>
@@ -393,6 +404,10 @@
     <message>
         <source>Options</source>
         <translation>Opsionet</translation>
+    </message>
+    <message>
+        <source>W&amp;allet</source>
+        <translation>Portofol</translation>
     </message>
     </context>
 <context>
@@ -414,12 +429,23 @@
         <source>Amount</source>
         <translation>Sasia</translation>
     </message>
+    <message>
+        <source>%1 and %2</source>
+        <translation>%1 dhe %2</translation>
+    </message>
+    </context>
+<context>
+    <name>QObject::QObject</name>
     </context>
 <context>
     <name>QRImageWidget</name>
     </context>
 <context>
     <name>RPCConsole</name>
+    <message>
+        <source>&amp;Information</source>
+        <translation>Informacion</translation>
+    </message>
     <message>
         <source>&amp;Open</source>
         <translation>&amp;Hap</translation>
@@ -436,16 +462,28 @@
         <source>Unknown</source>
         <translation>i/e panjohur</translation>
     </message>
-    </context>
+</context>
 <context>
     <name>ReceiveCoinsDialog</name>
+    <message>
+        <source>&amp;Amount:</source>
+        <translation>Shuma:</translation>
+    </message>
     <message>
         <source>&amp;Label:</source>
         <translation>&amp;Etiketë:</translation>
     </message>
+    <message>
+        <source>Clear</source>
+        <translation>Pastro</translation>
+    </message>
     </context>
 <context>
     <name>ReceiveRequestDialog</name>
+    <message>
+        <source>Copy &amp;Address</source>
+        <translation>&amp;Kopjo adresen</translation>
+    </message>
     <message>
         <source>Address</source>
         <translation>Adresë</translation>
@@ -470,10 +508,6 @@
         <translation>Etiketë</translation>
     </message>
     <message>
-        <source>Amount</source>
-        <translation>Sasia</translation>
-    </message>
-    <message>
         <source>(no label)</source>
         <translation>(pa etiketë)</translation>
     </message>
@@ -483,6 +517,10 @@
     <message>
         <source>Send Coins</source>
         <translation>Dërgo Monedha</translation>
+    </message>
+    <message>
+        <source>Insufficient funds!</source>
+        <translation>Fonde te pamjaftueshme</translation>
     </message>
     <message>
         <source>Amount:</source>
@@ -512,7 +550,7 @@
         <source>(no label)</source>
         <translation>(pa etiketë)</translation>
     </message>
-    </context>
+</context>
 <context>
     <name>SendCoinsEntry</name>
     <message>
@@ -522,10 +560,6 @@
     <message>
         <source>Pay &amp;To:</source>
         <translation>Paguaj &amp;drejt:</translation>
-    </message>
-    <message>
-        <source>Enter a label for this address to add it to your address book</source>
-        <translation>Krijoni një etiketë për këtë adresë që t'ja shtoni librit të adresave</translation>
     </message>
     <message>
         <source>&amp;Label:</source>
@@ -543,6 +577,17 @@
         <source>Alt+P</source>
         <translation>Alt+P</translation>
     </message>
+    <message>
+        <source>Pay To:</source>
+        <translation>Paguaj drejt:</translation>
+    </message>
+    <message>
+        <source>Enter a label for this address to add it to your address book</source>
+        <translation>Krijoni një etiketë për këtë adresë që t'ja shtoni librit të adresave</translation>
+    </message>
+</context>
+<context>
+    <name>SendConfirmationDialog</name>
     </context>
 <context>
     <name>ShutdownWindow</name>
@@ -564,10 +609,6 @@
     </context>
 <context>
     <name>SplashScreen</name>
-    <message>
-        <source>Artiqox Core</source>
-        <translation>Berthama Artiqox</translation>
-    </message>
     <message>
         <source>[testnet]</source>
         <translation>[testo rrjetin]</translation>
@@ -591,33 +632,33 @@
         <translation>%1 konfirmimet</translation>
     </message>
     <message>
-        <source>Date</source>
-        <translation>Data</translation>
-    </message>
-    <message>
-        <source>Amount</source>
-        <translation>Sasia</translation>
-    </message>
-    <message>
         <source>, has not been successfully broadcast yet</source>
         <translation>, nuk është transmetuar me sukses deri tani</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Data</translation>
     </message>
     <message>
         <source>unknown</source>
         <translation>i/e panjohur</translation>
     </message>
-</context>
+    <message>
+        <source>Transaction</source>
+        <translation>transaksionit</translation>
+    </message>
+    <message>
+        <source>Amount</source>
+        <translation>Sasia</translation>
+    </message>
+    </context>
 <context>
     <name>TransactionDescDialog</name>
-    <message>
-        <source>Transaction details</source>
-        <translation>Detajet e transaksionit</translation>
-    </message>
     <message>
         <source>This pane shows a detailed description of the transaction</source>
         <translation>Ky panel tregon një përshkrim të detajuar të transaksionit</translation>
     </message>
-</context>
+    </context>
 <context>
     <name>TransactionTableModel</name>
     <message>
@@ -627,6 +668,10 @@
     <message>
         <source>Type</source>
         <translation>Lloji</translation>
+    </message>
+    <message>
+        <source>Label</source>
+        <translation>Etiketë</translation>
     </message>
     <message>
         <source>Open until %1</source>
@@ -643,10 +688,6 @@
     <message>
         <source>Generated but not accepted</source>
         <translation>I krijuar por i papranuar</translation>
-    </message>
-    <message>
-        <source>Label</source>
-        <translation>Etiketë</translation>
     </message>
     <message>
         <source>Received with</source>
@@ -667,6 +708,10 @@
     <message>
         <source>(n/a)</source>
         <translation>(p/a)</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(pa etiketë)</translation>
     </message>
     </context>
 <context>
@@ -707,6 +752,10 @@
         <source>Address</source>
         <translation>Adresë</translation>
     </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Eksportimi dështoj</translation>
+    </message>
     </context>
 <context>
     <name>UnitDisplayStatusBarControl</name>
@@ -723,9 +772,21 @@
 </context>
 <context>
     <name>WalletView</name>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Eksporto të dhënat e skedës korrente në një skedar</translation>
+    </message>
     </context>
 <context>
-    <name>bitcoin-core</name>
+    <name>artiqox-core</name>
+    <message>
+        <source>Options:</source>
+        <translation>Opsionet:</translation>
+    </message>
+    <message>
+        <source>Artiqox Core</source>
+        <translation>Berthama Artiqox</translation>
+    </message>
     <message>
         <source>Information</source>
         <translation>Informacion</translation>
